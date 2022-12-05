deae094700a9b36654a3148b3b1bca8a8a20496e
pablohaller <pablo.haller.dev@gmail.com>
2022-12-05 02:34:20 -0300

    BFS Shortest Path

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ print('Obligatorio TDC\n')
 availableGraphs = AvailableGraphs.AvailableGraphs(Utils.GetGraphs())
 
 for graph in availableGraphs.graphs:
-    print(graph)
-    print("BFS(0)", GraphAlgorithms.BreadthFirstSearch(graph, "0"))
-    print("DFS(0)", GraphAlgorithms.DepthFirstSearch(graph, "0"))
-    print("Connected Components", GraphAlgorithms.GetConnectedComponents(graph))
+    # print(graph)
+    # print("BFS(0)", GraphAlgorithms.BreadthFirstSearch(graph, "0"))
+    # print("DFS(0)", GraphAlgorithms.DepthFirstSearch(graph, "0"))
+    # print("Connected Components", GraphAlgorithms.GetConnectedComponents(graph))
+    GraphAlgorithms.GetAllShortestPaths(graph, "0", "10")
     print("-------------------------")
 
