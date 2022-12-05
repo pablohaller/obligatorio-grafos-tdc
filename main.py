a6231c4dce4506436b1cbf5a918ac2ee570cfadf
Jose Curti <joc_qr@hotmail.com>
2022-12-05 19:14:50 -0300

    holis

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ for graph in availableGraphs.graphs:
     print("Get is connected:", GraphAlgorithms.GetIsConnected(graph))
     print("Get connected components amount:", GraphAlgorithms.GetConnectedComponentsAmount(graph))
     print("Shortest Path:", GraphAlgorithms.GetShortestPath(graph, "0", "11"))
-    print("Shortest Path Length:", GraphAlgorithms.GetShortestPathLength(graph, "0", "11"))
-    print("Is Shortest Path?:", GraphAlgorithms.CompareShortestPaths(graph, ['0', '2', '7', '6', '4', '5', '10', '11']))
+    print("Shortest Path Length:", GraphAlgorithms.GetShortestPathLength(graph, "0", "9"))
+    print("Is Shortest Path?:", GraphAlgorithms.CompareShortestPaths(graph, ['0','2','7','9']))
     print("-------------------------")
 
--- utils/Utils.py
+++ utils/Utils.py
@@ -13,6 +13,7 @@ def GetGraphs():
 
     for graphFile in graphFiles:
         graphPath = []
+        temp = []
         file = open(f"{graphsLocation}/{graphFile}", "r")
         fileContent = file.read()
         graphDictionary = json.loads(json.dumps(xmltodict.parse(fileContent)))
@@ -21,9 +22,16 @@ def GetGraphs():
             if key == "edge":
                 for edge in value:
                     graphPath.append(Path.Path(edge["@source"], edge["@target"]))
-
+                    temp.append(edge["@source"])
+                    temp.append(edge["@target"])
         graphList.append(Graph.Graph(graphFile, graphPath))
         file.close()
+
+    for key, value in graphDictionary["graphml"]["graph"].items():
+            if key == "node" :
+                for node in value:
+                    if node["@mainText"] not in temp:
+                        graphPath.append(Path.Path(node["@mainText"], node["@mainText"]))
 
     print("Graphs loaded...")
     return graphList
